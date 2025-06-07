cfd473ccf54555d352ab641d5ed356922dcfe858
minindu-alwis <minindualwis@gmail.com>
2025-06-07 18:52:11 +0530

    feat: update AdminRoute to use userRestaurants for conditional rendering

--- src/Routers/AdminRoute.jsx
+++ src/Routers/AdminRoute.jsx
@@ -5,13 +5,19 @@ import Admin from '../AdminComponent/Admin/Admin'
 import { useSelector } from 'react-redux'
 
 const AdminRoute = () => {
-  const {restaurant}=useSelector((store)=>store)
+  const { userRestaurants } = useSelector((store) => store.restaurant)
+  console.log(userRestaurants)
+
   return (
     <div>
-        <Routes>
-            <Route path='/*' element=
-            {restaurant.usersRestaurant ? <CreateRestaurantForm /> : <Admin />} />
-        </Routes>
+      <Routes>
+        <Route 
+          path='/*' 
+          element={!userRestaurants 
+            ? <CreateRestaurantForm /> 
+            : <Admin />} 
+        />
+      </Routes>
     </div>
   )
 }
